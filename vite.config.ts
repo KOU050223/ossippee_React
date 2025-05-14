372695e63464020d22dd6faf8c256fa6e465e22d
KOU050223 <kouhei20050223@icloud.com>
2025-05-14 12:16:52 +0900

    add テスト用sslプラグイン追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-  },
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
 })
