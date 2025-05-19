fdc501ce67c4403f3518dbfa7ed1ffde5e3f2500
KOU050223 <kouhei20050223@icloud.com>
2025-05-19 23:46:54 +0900

    add 自動遷移hooks追加 hooksのindexを追加

---
+++ src/hooks/index.ts
@@ -0,0 +1,19 @@
+import {
+  useDocument,
+  useDocumentRealtime,
+  useCollection,
+  useFirestore,
+  useUpdateField,
+} from "./useFirebase";
+import { useUserId } from "./useUserId";
+import { useCustomRouter } from "./useRouter";
+
+export {
+  useDocument,
+  useDocumentRealtime,
+  useCollection,
+  useFirestore,
+  useUpdateField,
+  useUserId,
+  useCustomRouter,
+};
