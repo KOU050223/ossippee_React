2f60bb03ffd04ed62b9834d7f926e912eb67657e
KOU050223 <kouhei20050223@icloud.com>
2025-05-19 19:39:18 +0900

    add firebaseに登録されているか確かめる

--- src/app/scenes/Entry.tsx
+++ src/app/scenes/Entry.tsx
@@ -1,9 +1,49 @@
+import { useDocument } from '@/hooks/useFirebase'
+import { useUserId } from '@/hooks/useUserId'
+import { Input, Avatar, Container } from '@chakra-ui/react'
+
+type UserData = {
+    id: string
+    userId: string
+    displayName: string
+    statusMessage: string
+    pictureUrl: string
+    gameState: string
+    nomiPoint: number
+}
+
 const Entry = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { userId, setUserId } = useUserId()
+
+    const { data, loading, error } = useDocument('users', userId)
+    const userData = data as UserData
+    return (
+        <Container>
+            <img src="./lineQR.png" alt="" />
+            <a>https://line.me/R/ti/p/%40974zguze</a>
+            <div>ユーザーネームを以下に入れてね</div>
+            <Input
+                id="userId"
+                type="text"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)}
+                placeholder="Enter user ID"
+            />
+            {loading && <p>Loading...</p>}
+            {!loading && data && 
+            <>
+                <div>認証されています</div>
+                <p>ユーザーネーム: {userData.displayName}</p>
+                <Avatar.Root size="md">
+                    <Avatar.Fallback name={userData.displayName} />
+                    <Avatar.Image src={userData.pictureUrl} />
+                </Avatar.Root>
+                {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+            </>
+            }
+            {error && <p>エラー: {error.message}</p>}
+        </Container>
+    )
 }
 
 export default Entry
