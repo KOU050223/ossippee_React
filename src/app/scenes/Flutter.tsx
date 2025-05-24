fc17a3093ab843bb1b1cd4ddb312a9cbc737a7ce
KOU050223 <kouhei20050223@icloud.com>
2025-05-24 14:15:42 +0900

    clear flutterコンポーネントを整理

--- src/app/scenes/Flutter.tsx
+++ src/app/scenes/Flutter.tsx
@@ -1,8 +1,4 @@
-import { Button } from '@chakra-ui/react';
-import { useUserId } from '@/hooks/useUserId';
-
 const Flutter = () => {
-  const { userId } = useUserId();
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       <iframe
@@ -10,8 +6,6 @@ const Flutter = () => {
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="Flutter App"
       />
-      {/* React 内で gameState 更新など他処理も可能 */}
-      <Button onClick={handleNextClick}>次へ</Button>
     </div>
   );
 };
